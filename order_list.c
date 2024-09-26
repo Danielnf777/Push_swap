163eeff3b2364b1826bb43053588b9f9b0cde3e1
Danielnf777 <159679983+Danielnf777@users.noreply.github.com>
2024-09-26 11:45:53 +0200

    Add files via upload

--- order_list.c
+++ order_list.c
@@ -53,40 +53,49 @@ t_list	*largest(t_list *lst_b)
 	}
 	return (larg);
 }
-
-void five(t_list **lst, t_list **lst_b)
+t_list	*smallest(t_list *lst)
 {
-	int	i;
-	t_list	*temp;
 	t_list	*small;
 
-	if (checker(*lst)== 0)
+	small = lst;
+	while (lst)
+	{
+		if (lst->content < small->content)
+			small = lst;
+		lst = lst->next;
+	}
+	return (small);
+}
+
+void five(t_list **lst, t_list **lst_b, int argc)
+{
+	t_list	*small;
+
+	if (sorted_list(*lst)== 0)
 		return ;
+	small = smallest(*lst);
 	while ((*lst)->content != small->content)
 	{
-		rra(lst);
+		if (position(*lst, small->content, argc) == 1)
+			ra(lst);
+		else
+			rra(lst);
 	}
 	pb(lst, lst_b);
+	argc--;
+	small = smallest(*lst);
 	while ((*lst)->content != small->content)
 	{
-		ra(lst);
+		if (position(*lst, small->content, argc) == 1)
+			ra(lst);
+		else
+			rra(lst);
 	}
 	pb(lst, lst_b);
 	three(lst);
 	pa(lst_b, lst);
 	pa(lst_b, lst);
-
-	//	temp = *lst;
-	//	  while (temp->next)
-	//	  {
-	//	  if ((temp)->content > (temp)->next->content)
-	//	  pb(lst, lst_b);
-	//	  else
-	//	  sa(lst);
-	//	  temp = temp->next;
-	//	  }
 }
-
 int	maximum(t_list	*lst)
 {
 	int	max;
@@ -115,100 +124,177 @@ int	minimum(t_list	*lst)
 	printf ("min = %d\n", min);
 	return (min);
 }
-int elements_chunk(t_list *lst, int chunk_max)
+int elements_chunk(t_list *lst, int chunk_max, int min)
 {
 	int	i;
 
 	i = 0;
 	while (lst)
 	{
-		if (lst->content > chunk_max)
+		if (lst->content < chunk_max && lst->content >= min)
 			i++;
 		lst = lst->next;
 	}
 	return (i);
 }
-int	position(t_list *lst, int numb)
+
+int	position_stack_a(t_list *lst, int chunk, int min, int argc)
+{
+	int	i;
+
+	i = 0;
+	while (lst)
+	{
+		if ( lst->content < chunk && lst->content >= min)
+		{
+			if ( i <= argc / 2)
+				return (1);
+			else
+				return (0);
+		}
+		i++;
+		lst = lst->next;
+	}
+	return (1);
+}
+
+
+
+int	position(t_list *lst, int numb, int argc)
 {	
 	int	i;
 
 	i = 0;
 	while (lst)
 	{
-		printf("position = %d\n", i);
 		if (lst->content == numb)
-			return (i);
+		{
+			if (i <= argc / 2)
+				return (1);
+			else
+				return (0);
+		}
 		i++;
 		lst = lst->next;
 	}
-	return (i);
+	return (1);
 }
 void	hundred(t_list **lst, t_list **lst_b, int argc)
 {
 	int	max;
 	int	min;
-	int	chunk_1_min;
-	int	chunk_max;
+	int	portion;
+	int	chunk;
 	int	i;
-	int	limit;
+	int	copy;
 
 	t_list *larg;
 	t_list *temp;
 
 	min = minimum(*lst);
 	max = maximum(*lst);
-	chunk_max = max - ((max - min) / 3);
-	limit = chunk_max;
+	portion = (max - min) / 5;
+	chunk = min + portion;
 	temp = *lst;
-	//	while ((*lst)->next)
-	printf("111argc = %d\n", argc);
-	printf("chunk_max = %d\n", chunk_max);
-	while (argc > 0)
+	copy = argc;
+	//	while (argc > 1)
+	while (i > 0)
 	{
-		i = elements_chunk(*lst, chunk_max);
-		printf("chunk = %d\n", i);
-		printf("argc= %d\n", argc);
+		i = elements_chunk(*lst, chunk, min);
 		while (i > 0)
-		{	
-			while ((*lst)->content < limit && (*lst)->content >= min)
+		{
+			if (!*lst_b && sorted_list(*lst) == 0)
+				return ;
+
+			if ((*lst)->content >= min && (*lst)->content < chunk )
 			{
-				//	if (position(*lst, chunk_max) > argc / 2) 
-				//		ra(lst);
-				//	else
-				rra(lst);
+				if (!*lst_b)
+				{
+					pb(lst, lst_b);
+					i--;
+					argc--;
+				}
+
+				else if ((*lst_b) && (*lst)->content > (*lst_b)->content) 
+				{
+					pb(lst, lst_b);
+					i--;
+					argc--;
+				}
+				else	
+				{
+
+				
+					temp = *lst_b;
+					while (temp && (*lst)->content < (*lst_b)->content)
+					{	
+						rb(lst_b);
+						if ((*lst)->content > (*lst_b)->content)
+						{
+							pb(lst, lst_b);
+							i--;
+							argc--;
+						}
+						temp = temp->next;
+					}
+				}
 			}
-			pb(lst, lst_b);
-			i--;
-			argc--;
+			else if (position_stack_a(*lst, chunk, min, argc) == 1)
+				ra(lst);
+			else
+				rra(lst);
 
 		}
+		temp = *lst_b;
+//		if (argc < 3)
+//			{
+//				pb(lst, lst_b);
+//				pb(lst, lst_b);
+//			}
+		while ((temp)->next)
+		{
+			printf("temp %d\n", (temp)->content);
+			temp = temp->next;
+		}
+/*
 		larg = largest(*lst_b);
-	//	while (*lst_b)
-	//	{
-	//		printf("list_B:%d\n", (*lst_b)->content);
-	//		*lst_b = (*lst_b)->next;
-	//	}
 		while (*lst_b)
 		{
 			while((*lst_b)->content != larg->content)
-				rrb(lst_b);
+			{
+				if (position(*lst_b, larg->content, copy) == 1)
+						rb(lst_b);
+				else
+					rrb(lst_b);
+			}
+
+	//			rb(lst_b);
+			while ((*lst_b) && (*lst)->content < chunk)
+			{
+				ra(lst);
+				*lst = (*lst)->next;
+			}
 			pa(lst_b, lst);
-			//	*lst_b = (*lst_b)->next;
+			copy--;
 			larg = largest(*lst_b);
 		}
-		while (*lst)
+		temp = *lst;
+		while ((temp)->next)
 		{
-			printf("list:%d\n", (*lst)->content);
-			*lst = (*lst)->next;
+			printf("temp %d\n", (temp)->content);
+			temp = temp->next;
 		}
-		min = limit;
-		limit = limit + chunk_max;
-		i = elements_chunk(*lst, chunk_max);
+
+		min = chunk;
+		chunk = chunk + portion;
+//		i = elements_chunk(*lst, chunk);
 
 	}
 	printf("min = %d\n", min);
 	printf("max = %d\n", max);
-	printf("chunk_max = %d\n", chunk_max);
 	printf("argc = %d\n", argc);
 
+}
+*/
+	}
 }
